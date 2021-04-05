fc92c4b0a3fc0120c5807095414a6614e1829ba1
davidpitutiu <dpitutiu@icloud.com>
2021-04-05 13:28:18 +0300

    Update index.php

--- 26_Final/Student/index.php
+++ 26_Final/Student/index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link href="/assets/styles.css" rel="stylesheet">
+    <link href="/assets/styles.css" rel="stylesheet">
+    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,55 +10,38 @@
 </head>
 <body id="final-exemple">
 
+<div class="wrapper">
+    <div id="banner">
+    </div><!--Banner ends-->
+    <div id="nav">
+    </div><!--Nav ends -->
+    <div class="content">
+        <div id="footer" class="cf">
+        <div class="column three">
+            <strong>Phone</strong>
+            808.529.3819
+        </div> <!--Comlun ends-->
+        <div class="column three">
+            <strong>Location</strong>
+            123 Kapiolani Boulevard<br>
+                Honolulu, HI
+        </div> <!--Comlun ends-->
+        <div class="column three last">
+        <strong>Hours</strong>
+        <em>Tuesday - Thursday</em><br>
+                1:00pm - 9:00pm<br><br>
+                
+                <em>Friday - Saturday</em><br>
+                4:00pm - 11:00pm<br><br>
+                
+                <em>Sunday - Monday</em><br>
+                Closed<br>
+                
+                <br>
+        </div> <!--Comlun ends-->
+        </div> <!--Footer ends-->
+    </div> <!--Content ends -->
+</div>  <!-- Wrapper ends -->
 
-    
-    <div class="wrapper">
-
-        <div id="banner">
-
-        </div><!--Banner ends-->
-
-        <div id="nav">
-
-        </div><!--Nav ends -->
-
-        <div class="content">
-
-            <div id="footer" class="cf">
-
-            <div class="column three">
-                <strong>Phone</strong>
-                808.529.3819
-            </div> <!--Comlun ends-->
-
-            <div class="column three">
-                <strong>Location</strong>
-                123 Kapiolani Boulevard<br>
-					Honolulu, HI
-
-            </div> <!--Comlun ends-->
-
-            <div class="column three last">
-            <strong>Hours</strong>
-            <em>Tuesday - Thursday</em><br>
-					1:00pm - 9:00pm<br><br>
-					
-					<em>Friday - Saturday</em><br>
-					4:00pm - 11:00pm<br><br>
-					
-					<em>Sunday - Monday</em><br>
-					Closed<br>
-					
-					<br>
-
-            </div> <!--Comlun ends-->
-
-            </div> <!--Footer ends-->
-
-        </div> <!--Content ends -->
-
-    </div>  <!-- Wrapper ends -->
-    
 </body>
-
 </html>
