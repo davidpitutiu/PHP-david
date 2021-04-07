4a9b9bd503c073c196ef08311b830dae5ea8baf2
davidpitutiu <dpitutiu@icloud.com>
2021-04-07 12:30:38 +0300

    Update index.php

--- 26_Final/Student/index.php
+++ 26_Final/Student/index.php
@@ -1,60 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link href="/assets/styles.css" rel="stylesheet">
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body id="final-exemple">
-    <?php
-        $a=5;
-    ?>
-
-    <div class="wrapper">
-
-        <div id="banner">
-
-        </div><!--Banner ends-->
-
-        <div id="nav">
-
-        </div><!--Nav ends -->
-
-        <div class="content">
-
-            <div id="footer" class="cf">
-
-            <div class="column three">
-
-                <strong>
-                    Phone
-                </strong>
-                    808.529.3819
-            </div> <!--Comlun ends-->
-        <div class="column three">
-            <strong>Location</strong>
-            123 Kapiolani Boulevard<br>
-                Honolulu, HI
-        </div> <!--Comlun ends-->
-        <div class="column three last">
-        <strong>Hours</strong>
-        <em>Tuesday - Thursday</em><br>
-                1:00pm - 9:00pm<br><br>
-                
-                <em>Friday - Saturday</em><br>
-                4:00pm - 11:00pm<br><br>
-                
-                <em>Sunday - Monday</em><br>
-                Closed<br>
-                
-                <br>
-        </div> <!--Comlun ends-->
-        </div> <!--Footer ends-->
-    </div> <!--Content ends -->
-</div>  <!-- Wrapper ends -->
-
-</body>
-</html>
+<?php
+    define('TITLE',"Home|Franklin's Fine Dining");
+    include('includes/header.php');
+?>
+<div id ='philosophy'>
+    <hr>
+        <h1>Franklin's Philosophy of Fine Dining</h1>
+		<p>Here at Franklin's, we know that good food isn't just about how expensive the dish is. We're not pompous, we're proud. We're proud of our work, our quality, our environment, and our love for food and family.</p>
+		<p>Oh, and you can call us Frankie's. You're family here.</p>
+		
+    </hr>
+</div>
+<?php
+    include('includes/footer.php');
+?>
