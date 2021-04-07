7040e69d0ec0d36ceb7accaa614d3591257bac83
davidpitutiu <dpitutiu@icloud.com>
2021-04-07 12:23:22 +0300

    Create header.php

---
+++ 26_Final/Student/includes/header.php
@@ -0,0 +1,33 @@
+<?php
+    $companyName="Franklin's Fine Dining";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="/assets/styles.css" rel="stylesheet">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo TITLE; ?></title>
+</head>
+<body id="final-exemple">
+
+
+    <div class="wrapper">
+
+        <div id="banner">
+            <a href="/" title="Return to Home">
+                <img src="img/banner.png" alt="Franklin's Fine Dining">
+            </a>
+        </div><!--Banner ends-->
+
+        <div id="nav">
+
+            <?php 
+                include('includes/nav.php');
+            ?>
+
+        </div><!--Nav ends -->
+
+        <div class="content">
